837987544ae94312073fc2b630ba168888c542fe
mooncat01 <lrel1823@gmail.com>
2024-09-13 13:20:53 +0900

    modifify 2file

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import prime_number as pn
 if __name__ == '__main__':
     pn.is_prime_number(1)
     pn.is_prime_number(13)
-    pn.is_prime_number(24)
     pn.is_prime_number(31)
-    pn.is_prime_number(45)
+    pn.is_prime_number(48)
     pn.is_prime_number(97)
